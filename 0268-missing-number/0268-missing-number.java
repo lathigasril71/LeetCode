6205d4b2319fe0413321e582f86ab5021efb1f13
lathigasril71 <lathigal71@gmail.com>
2026-06-27 00:26:06 +0530

    Time: 0 ms (100%), Space: 47.2 MB (81.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        List<Integer> ol = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        for(int i=0;i<=n;i++)
-        if(!ol.contains(i))
-        return i;
+        int hash[] = new int[nums.length+1];
+        for(int i=0;i<n;i++)
+            hash[nums[i]] = 1;
+    
+        for(int i=0;i<n+1;i++)
+            if(hash[i] == 0)
+            return i;
 
         return -1;
+
+
+
     }
 }
