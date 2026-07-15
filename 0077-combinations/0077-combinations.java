02bfc6254d1e73169cf86275c2d5adcd542782e1
Lathiga Sri L <lathigal71@gmail.com>
2026-07-15 23:14:24 +0530

    Time: 22 ms (20.15%), Space: 98 MB (65.9%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+       int nums[] = new int[n];
+       for(int i=1;i<=n;i++)
+       nums[i-1] = i;
+
+       List<List<Integer>> list = new ArrayList<>();
+       List<Integer> ds = new ArrayList<>();
+       func(0,list,ds,nums,k);
+       return list; 
+    }
+
+    void func(int ind,List<List<Integer>> list,List<Integer> ds,int[] nums,int k)
+    {
+        if(ds.size() == k)
+        {
+        list.add(new ArrayList<>(ds));   
+        return;
+        }
+
+        if(ind == nums.length)
+        return;
+
+        ds.add(nums[ind]);
+        func(ind+1,list,ds,nums,k);
+        ds.remove(ds.size()-1); 
+        func(ind+1,list,ds,nums,k);
+    }
+}
