efcc2770a64e39723b04df440fd1d3dceb374d27
Lathiga Sri L <lathigal71@gmail.com>
2026-07-20 04:59:48 +0530

    Time: 0 ms (100%), Space: 43.8 MB (62.83%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       int j=0; 
-      for(int i=m;i<nums1.length;i++)
-      nums1[i] = nums2[j++]; 
-
-        Arrays.sort(nums1);
+        // Edge cases
+        if (n == 0) return;
+        
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+        // If nums2 still has elements
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
